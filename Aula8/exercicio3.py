3d3206f7a4924fc84fa6d58216af60f80d0a9e51
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-10-17 21:36:13 -0300

    Create exercicio3.py

---
+++ Aula8/exercicio3.py
@@ -0,0 +1,23 @@
+def is_leap_year(year):
+   if (year % 4 ==0 and not year % 100 == 0) or year % 400 == 0:
+       return True
+       
+    return False
+
+
+
+def main():
+    try:
+        year = int(input("Digite um ano:"))
+        
+        result = is_leap_year(year)
+        
+        print("é bissexto?",result)
+    except ValueError:
+        print('[Error] Valor digitado inválido')
+        
+    except BaseException as error:
+        print('[Error Aconteceu um erro:', error)
+    
+    
+main()
