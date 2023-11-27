5406c257c75ca152208b43c644220d0b9f0a7f7c
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-11-26 23:05:20 -0300

    Create menu.py

---
+++ trabalhoG2/menu.py
@@ -0,0 +1,7 @@
+def show_menu():
+    print("\033[95m\t - - - - - T E R M O - - - - -\n\033[0m")
+    print("\t Bem vindo(a) ao menu principal")
+    print("\t 1. jogar")
+    print("\t 2. resetar palvras usadas")
+    print("\t 3. sair")
+    print()
