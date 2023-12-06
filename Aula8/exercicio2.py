79500acf73a7a5dc37a7fd4aafd4464ea24af402
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-12-06 20:52:03 -0300

    Update exercicio2.py

--- Aula8/exercicio2.py
+++ Aula8/exercicio2.py
@@ -1,8 +1,6 @@
 class InvalidMonthException(BaseException):
     pass
 
-
-
 def get_month_name(month):
     try: 
         months = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
@@ -13,22 +11,26 @@ def get_month_name(month):
     except BaseException as error:
         print("ocorreu um erro")
         
-        
-    
-    
 def main():
-    try: 
-        month = int(input("Digite o mês: "))
-        
-        if month > 12 or month < 1:
-            raise InvalidMonthException()
-        
-        month_name = get_month_name(month)
-        
-        print(f"nome do mês: {month_name}")
-    except ValueError:
-        print("VocÊ deve digitar somente números inteiros")
-    except BaseException as error: 
-        print("Ocorreu um erro", error)
+    while True:
+        try: 
+            month = int(input("Digite o mês: "))
+            
+            if month > 12 or month < 1:
+                raise InvalidMonthException()
+            
+            month_name = get_month_name(month)
+            
+        except ValueError:
+            print("Você deve digitar somente números inteiros. Tente novamente! ")
+            print()
+            continue
+        except BaseException as error: 
+            print("Ocorreu um erro, Tente Novamente! ", error)
+            print()
+            continue
+        else:
+            print(f"nome do mês: {month_name}")
+        break
         
 main()
