e5ca835f664a6d2f67a2dd821a32f315ef793ab4
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-12-07 19:27:50 -0300

    Update exercicio3.py

--- aula9/exercicio3.py
+++ aula9/exercicio3.py
@@ -1,1 +1,22 @@
+class NegativeNumberError(Exception):
+    pass
 
+def calculate_square_root():
+    number = int(input("Insira um número: "))
+    
+    if number < 0:
+        raise NegativeNumberError("Erro! A raiz quadrada de números negativos é inválida.")
+    else:
+        square_root = number ** 0.5
+        print(f"A raiz quadrada de {number} é {square_root:.2f}")
+
+def main():
+    try:
+        calculate_square_root()
+    except NegativeNumberError as error:
+        print(error)
+    except Exception as error:
+        print(f"Ocorreu um erro: {error}")
+
+
+main()
