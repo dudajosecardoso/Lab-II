8ad7a8239801f37d7b37a2897ba20e69d2fb308d
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-12-07 19:13:02 -0300

    Update exercicio4.py

--- aula9/exercicio4.py
+++ aula9/exercicio4.py
@@ -1,1 +1,16 @@
+def find_element(my_list, number):
+    try:
+        result = my_list[number]
+        print(result)
+    except IndexError:
+        print("Erro! O índice informado não existe na lista.")
 
+def main():
+    try:
+        my_list = ["batata", "cebola", "tomate", "alface", "cenoura"]
+        number = int(input("Insira o índice desejado: "))
+        find_element(my_list, number)
+    except ValueError:
+        print("Erro! Por favor, insira um número inteiro como índice.")
+
+main()
