d0f9f30b6a2cbb402315ca3feeb63319d65ba1aa
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2023-12-07 19:46:50 -0300

    Update exercicio2.py

--- aula9/exercicio2.py
+++ aula9/exercicio2.py
@@ -1,1 +1,18 @@
+def give_note():
+    try:
+        note = int(input("Dê sua nota de 0 a 10 para nosso produto: "))
 
+        if note < 0:
+            raise ValueError("Erro! Não é permitido inserir números negativos.")
+        
+        assert 0 <= note <= 10
+        print("Obrigada pela avaliação!")
+
+    except ValueError:
+        print("Por favor utilize apenas números positivos.")
+    except AssertionError:
+        print("O número está acima do permitido!")
+
+def main():
+    give_note()
+main()
